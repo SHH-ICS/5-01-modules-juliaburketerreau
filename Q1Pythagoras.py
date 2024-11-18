6c29a50c08e0dc38265b75c2ecdc4c062ff52329
Julia Burke-Terreau <julia.burke-terreau@stu.ocsb.ca>
2024-11-18 15:14:18 +0000

    added loop

--- Q1Pythagoras.py
+++ Q1Pythagoras.py
@@ -2,11 +2,15 @@
 # Remember to use prompts for the input and labels for the output. Use the formula a2 + b2 = c2 to calculate your answer.
 redo = True
 while (redo):
-  a = float(input( "Put in sidelength 1 " ))
-  b = float(input( "Put in sidelength 2 " ))
-  if a != float and b != float:
-    print("Put in a number or a decimal")
-  else:
-    redo = False
+  a = input( "Put in sidelength 1 " )
+  b = input( "Put in sidelength 2 " )
+  try:
+    a = float(a)
+    b = float(b)
+    if a >= 1 and b >= 1:
+        redo = False       
+  except: 
+    redo = True
+    print("Try again! Both inputs must be a positive number.")
 hyp = a **2 + b**2
-print(hyp)
+print("The hypotenuse is", hyp)
--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,2 +1,5 @@
 # Create a program that accepts 2 numbers from the user. 
 # Your program will output a random number between the range given by the user.
+n = input()
+n2 = input()
+for i in range n-n2: 
