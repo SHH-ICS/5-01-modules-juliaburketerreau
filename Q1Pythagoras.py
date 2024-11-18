4e7db584be7b318ef70e30f8736247750ab74d25
Julia Burke-Terreau <julia.burke-terreau@stu.ocsb.ca>
2024-11-18 17:07:09 +0000

    added square root

--- Q1Pythagoras.py
+++ Q1Pythagoras.py
@@ -1,9 +1,10 @@
 # Create a program that will accept the two legs of a right-angle triangle, a and b, and display the length of the hypotenuse, c. 
 # Remember to use prompts for the input and labels for the output. Use the formula a2 + b2 = c2 to calculate your answer.
+import math
 redo = True
 while (redo):
-  a = input( "Put in sidelength 1 " )
-  b = input( "Put in sidelength 2 " )
+  a = input( "Put in sidelength 1: " )
+  b = input( "Put in sidelength 2: " )
   try:
     a = float(a)
     b = float(b)
@@ -13,4 +14,5 @@ while (redo):
     print("Try again! Both inputs must be a positive number.")
     redo = True
 hyp = a **2 + b**2
+hyp = math.sqrt(hyp)
 print("The hypotenuse is", hyp)
