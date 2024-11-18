3a1e567c21a9cf53737bfd8f042daa804d79c712
Julia Burke-Terreau <julia.burke-terreau@stu.ocsb.ca>
2024-11-18 15:44:04 +0000

    trying to solve

--- Q3Addition.py
+++ Q3Addition.py
@@ -1,2 +1,11 @@
 # Create a program that will ask the user an addition question. 
 # The program will generate two random numbers between 1 and 100, and display them as an addition question with appropriate prompts.
+import random
+a = random.randint(1,100)
+b = random.randint(1,100)
+print("What is the answer to", a , "+" , b)
+ans = input()
+if ans == a + b:
+    print("That is correct!")
+else:
+    print("Try again...")
