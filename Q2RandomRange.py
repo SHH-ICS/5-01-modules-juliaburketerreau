d9af2ba6db517f3e2455f74aa958d2ed6da2f0b7
Julia Burke-Terreau <julia.burke-terreau@stu.ocsb.ca>
2024-11-19 15:49:28 +0000

    half works

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -12,6 +12,4 @@ while redo:
   except:
     print("Please enter a valid number")
     redo = True
-print(random.randint(a,b))
-if a < b:
- print(random.randint(b,a))
+print(random.randint(a,b)) 
--- Q4Picture.py
+++ Q4Picture.py
@@ -1,2 +1,3 @@
 # Using the pygame library, draw a simple picture. 
 # It can be anything you like, but you must use at least 2 different types of shapes and 3 different colors.
+
